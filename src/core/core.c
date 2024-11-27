b8e420d0d1e4e5f779c0095e3e8698ebf2c6cfef
jed <jedfakhfekh@gmail.com>
2024-11-27 17:44:28 +0900

    Add assert and base strategy & instance

--- src/core/core.c
+++ src/core/core.c
@@ -1,5 +1,6 @@
 #include "core.h"
 #include <stdlib.h>
+#include <assert.h>
 
 at_id at_new_id(){
     static at_id id = 0;
@@ -7,20 +8,67 @@ at_id at_new_id(){
 }
 
 void at_init_symbol(at_symbol *symbol, c8 *name, c8 *exchange, c8 *currency, sz tick_count){
+    assert(symbol && name && exchange && currency);
     symbol->name = name;
     symbol->exchange = exchange;
     symbol->currency = currency;
     symbol->tick_count = tick_count;
-    symbol->ticks = (at_tick *)malloc(sizeof(at_tick) * tick_count);
+    if (tick_count > 0){
+        symbol->ticks = (at_tick *)malloc(sizeof(at_tick) * tick_count);
+    }
 }
 
-void at_free_symbol(at_symbol *symbol)
-{
+void at_add_tick(at_symbol *symbol, at_tick *tick){
+    assert(symbol && tick);
+    symbol->tick_count++;
+    symbol->ticks = (at_tick *)realloc(symbol->ticks, sizeof(at_tick) * symbol->tick_count);
+    symbol->ticks[symbol->tick_count - 1] = *tick;
+}
+
+at_tick* at_get_tick(at_symbol *symbol, u32 index){
+    assert(symbol);
+    if (index < symbol->tick_count){
+        return &symbol->ticks[symbol->tick_count - index - 1];
+    }
+    return NULL;
+}
+
+at_tick* at_get_last_tick(at_symbol *symbol){
+    return at_get_tick(symbol, 0);
+}
+
+at_candle* at_get_candles(at_symbol *symbol, u32 period){
+    assert(symbol && period > 0);
+    if (symbol->tick_count % period != 0){
+        return NULL;
+    }
+
+    at_candle* candles = (at_candle *)malloc(sizeof(at_candle) * (symbol->tick_count / period));
+    for (u32 i = 0; i < symbol->tick_count; i++){
+        if (i % period == 0){
+            at_candle candle = {0};
+            candle.open = symbol->ticks[i].price;
+            candle.close = symbol->ticks[i + period - 1].price;
+            candle.volume = 0;
+            candle.adj_close = 0;
+            for (u32 j = i; j < i + period; j++){
+                candle.volume += symbol->ticks[j].volume;
+                candle.adj_close += symbol->ticks[j].price;
+            }
+            candle.adj_close /= period;
+            candles[i / period] = candle;
+        }
+    }
+    return candles;
+}
+
+void at_free_symbol(at_symbol *symbol){
+    assert(symbol);
     free(symbol->ticks);
 }
 
-void at_init_account(at_account *account, f64 balance)
-{
+void at_init_account(at_account *account, f64 balance){
+    assert(account);
     account->id = at_new_id();
     account->balance = balance;
     account->equity = balance;
@@ -34,6 +82,7 @@ void at_free_account(at_account *account){
 }
 
 void at_init_trade(at_trade *trade, at_id account_id, c8 *symbol, u32 volume, f64 open_price, u32 open_time){
+    assert(trade && symbol);
     trade->id = at_new_id();
     trade->account_id = account_id;
     trade->symbol = symbol;
@@ -52,6 +101,7 @@ void at_free_trade(at_trade *trade){
 }
 
 void at_init_order(at_order *order, at_id account_id, c8 *symbol, u32 volume, f64 price, u32 time){
+    assert(order && symbol);
     order->id = at_new_id();
     order->account_id = account_id;
     order->symbol = symbol;
@@ -61,15 +111,15 @@ void at_init_order(at_order *order, at_id account_id, c8 *symbol, u32 volume, f6
 }
 
 void at_add_order(at_account *account, at_order *order){
+    assert(account && order);
     account->margin += order->volume * order->price;
     account->free_margin = account->balance - account->margin;
     account->margin_level = account->equity / account->margin;
 }
 
 void at_remove_order(at_account *account, at_order *order){
+    assert(account && order);
     account->margin -= order->volume * order->price;
-    account->free_margin = account->balance - account->margin;
-    account->margin_level = account->equity / account->margin;
 }
 
 void at_update_order(at_account *account, at_order *order, f64 price){
@@ -80,6 +130,7 @@ void at_update_order(at_account *account, at_order *order, f64 price){
 }
 
 void at_close_order(at_account *account, at_order *order, f64 price){
+    assert(account && order);
     account->margin -= order->volume * order->price;
     account->margin += order->volume * price;
     account->free_margin = account->balance - account->margin;
@@ -88,6 +139,7 @@ void at_close_order(at_account *account, at_order *order, f64 price){
 }
 
 void at_cancel_order(at_account *account, at_order *order){
+    assert(account && order);
     account->margin -= order->volume * order->price;
     account->free_margin = account->balance - account->margin;
     account->margin_level = account->equity / account->margin;
@@ -95,4 +147,38 @@ void at_cancel_order(at_account *account, at_order *order){
 
 void at_free_order(at_order *order){
     // Nothing to free
+}
+
+void at_init_instance(at_instance *instance, at_strategy *strategy, at_symbol *symbol, at_account *account){
+    assert(instance && strategy && symbol && account);
+    instance->id = at_new_id();
+    instance->strategy = strategy;
+    instance->symbol = symbol;
+    instance->account = account;
+    instance->trades = NULL;
+    instance->trade_count = 0;
+    instance->orders = NULL;
+    instance->order_count = 0;
+}
+
+void at_free_instance(at_instance *instance){
+    assert(instance);
+    free(instance->trades);
+    free(instance->orders);
+}
+
+void at_tick_instance(at_instance *instance, at_tick *tick){
+    assert(instance && tick);
+    instance->strategy->on_tick(instance, tick);
+    instance->strategy->on_candle(instance, at_get_candles(instance->symbol, 5));
+
+    for (u32 i = 0; i < instance->order_count; i++){
+        at_order *order = &instance->orders[i];
+        if (order->symbol == instance->symbol->name){
+            if (order->price >= tick->price){
+                at_close_order(instance->account, order, tick->price);
+                at_remove_order(instance->account, order);
+            }
+        }
+    }
 }
--- src/core/core.h
+++ src/core/core.h
@@ -6,20 +6,25 @@
 typedef u32 at_id;
 
 typedef struct at_tick {
+    f64 price;
+    f64 volume;
+} at_tick;
+
+typedef struct at_candle {
     f64 open;
     f64 high;
     f64 low;
     f64 close;
     f64 volume;
     f64 adj_close;
-} at_tick;
+} at_candle;
 
 typedef struct at_symbol {
     c8* name;
     c8* exchange;
     c8* currency;
     sz tick_count;
-    struct at_tick *ticks;
+    at_tick *ticks;
 } at_symbol;
 
 typedef struct at_account {
@@ -54,9 +59,35 @@ typedef struct at_order {
     u32 time;
 } at_order;
 
+typedef struct at_instance {
+    at_id id;
+    struct at_strategy* strategy;
+    at_symbol* symbol;
+    at_account* account;
+    at_trade* trades;
+    u32 trade_count;
+    at_order* orders;
+    u32 order_count;
+} at_instance;
+
+typedef void (*on_start_callback)(at_instance* instance);
+typedef void (*on_tick_callback)(at_instance* instance, at_tick* tick);
+typedef void (*on_candle_callback)(at_instance* instance, at_candle* candle);
+typedef struct at_strategy {
+    at_id id;
+    c8* name;
+    on_start_callback on_start;
+    on_tick_callback on_tick;
+    on_candle_callback on_candle;
+} at_strategy;
+
 extern at_id at_new_id();
 
 extern void at_init_symbol(at_symbol *symbol, c8 *name, c8 *exchange, c8 *currency, sz tick_count);
+extern void at_add_tick(at_symbol *symbol, at_tick *tick);
+extern at_tick* at_get_tick(at_symbol *symbol, u32 index);
+extern at_tick* at_get_last_tick(at_symbol *symbol);
+extern at_candle* at_get_candles(at_symbol *symbol, u32 period);
 extern void at_free_symbol(at_symbol *symbol);
 
 extern void at_init_account(at_account *account, f64 balance);
@@ -72,3 +103,11 @@ extern void at_update_order(at_account *account, at_order *order, f64 price);
 extern void at_close_order(at_account *account, at_order *order, f64 price);
 extern void at_cancel_order(at_account *account, at_order *order);
 extern void at_free_order(at_order *order);
+
+extern void at_init_strategy(at_strategy *strategy, c8 *name, on_start_callback on_start, on_tick_callback on_tick, on_candle_callback on_candle);
+extern void at_free_strategy(at_strategy *strategy);
+
+extern void at_init_instance(at_instance *instance, at_strategy *strategy, at_symbol *symbol, at_account *account);
+extern void at_free_instance(at_instance *instance);
+extern void at_add_trade(at_instance *instance, at_trade *trade);
+extern void at_tick_instance(at_instance *instance, at_tick *tick);
